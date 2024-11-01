aabb840e44a30f086362e4f484eb42a0bb312430
Paul Riddell <paul.riddell@space-bx.co.uk>
2024-11-01 15:47:34 +0000

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from "./components/Card"
+import data from './data'
 
-function App() {
-  const [count, setCount] = useState(0)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+//Spread in object literals using the ...item notation
+export default function App() {
+
+  const cards = data.map((item) => {
+    return <Card
+      key={item.id}
+      {...item} />
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cards}
+      </section>
+    </div>
   )
 }
-
-export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+
+export default function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            {/* if truthy, then render the div */}
+            <img className="card--image" src={`../images/${props.coverImg}`} />
+            <div className="card--stats">
+                <img className="card--star" src='./images/star.png' />
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span>/ person</p>
+        </div>
+    )
+}
